8f59de68f805daacb1f069879177b2d4f26a1532
RithickB <49742736+RithickB@users.noreply.github.com>
2019-12-04 14:33:58 +0530

    Create LList_Delete.js

---
+++ LList_Delete.js
@@ -0,0 +1,106 @@
+/*
+Question in HackerRank:
+Delete a Node from given Index
+*/
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// My Code Begins
+function deleteNode(head, position) {
+    if(position===0){
+        head=head.next;
+    }else{
+        let current=head;
+        let previous,it=0;
+        while(it<position){
+            previous=current;
+            current=current.next;
+            it++;
+        }
+        previous.next=current.next;
+    }
+    return head;
+}
+// My Code Ends
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const llistCount = parseInt(readLine(), 10);
+
+    let llist = new SinglyLinkedList();
+
+    for (let i = 0; i < llistCount; i++) {
+        const llistItem = parseInt(readLine(), 10);
+        llist.insertNode(llistItem);
+    }
+
+    const position = parseInt(readLine(), 10);
+
+    let llist1 = deleteNode(llist.head, position);
+
+    printSinglyLinkedList(llist1, " ", ws)
+    ws.write("\n");
+
+    ws.end();
+}
