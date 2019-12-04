8b85b68afec5f924f8ebd1492259b34b68bc5bef
RithickB <49742736+RithickB@users.noreply.github.com>
2019-12-04 13:04:41 +0530

    Create LList_ReversePrint.js

---
+++ LList_ReversePrint.js
@@ -0,0 +1,101 @@
+/*
+Question in Hackerrank:
+Print in Reverse order
+*/
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep) {
+    while (node != null) {
+        process.stdout.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            process.stdout.write(sep);
+        }
+    }
+}
+
+//My Code Begins
+function reversePrint(head) {
+    if(head!=null){
+        let a=[];
+        let current=head;
+        while(current)
+        {
+            a.unshift(current.data);
+            current=current.next;
+        }
+        for(let key of a)
+        {
+            console.log(key);
+        }
+    }
+}
+//My Code Ends
+
+
+function main() {
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const llistCount = parseInt(readLine(), 10);
+
+        let llist = new SinglyLinkedList();
+
+        for (let i = 0; i < llistCount; i++) {
+            const llistItem = parseInt(readLine(), 10);
+            llist.insertNode(llistItem);
+        }
+
+        reversePrint(llist.head);
+    }
+}
