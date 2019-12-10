80c8a2622ef3926b37eeb3006fc51f7861d2adcd
RithickB <49742736+RithickB@users.noreply.github.com>
2019-12-10 20:15:33 +0530

    Create DLList_Insert_Node.js

---
+++ DLList_Insert_Node.js
@@ -0,0 +1,141 @@
+/*
+Question in HackerRank:
+Inserting a Node Into a Sorted Doubly Linked List
+*/'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const DoublyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+        this.prev = null;
+    }
+};
+
+const DoublyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        let node = new DoublyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printDoublyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode next;
+ *     DoublyLinkedListNode prev;
+ * }
+ *
+ */
+function sortedInsert(head, data) {
+    let node=new DoublyLinkedListNode(data);
+    if(head===null){
+        return node;
+    }
+    //To Check that the created node is the smaller than the given node
+    else if(node.data<head.data){
+        node.prev=null;
+        node.next=head;
+        head.prev=node;
+        head=node;
+    }
+    else{
+    let current=head;
+    let previous=null;
+    while(current&&current.data<node.data){
+        previous=current;
+        current=current.next;
+    }
+    //To check that the created node is the larger than the given node
+    if(current===null){
+        previous.next=node;
+        node.prev=previous;
+    }
+    //To check that the created node is the middle value the given node
+    else{
+        previous.next=node;
+        node.prev=previous;
+        node.next=current;
+        current.prev=node;
+    }
+    }
+    return head;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const llistCount = parseInt(readLine(), 10);
+
+        let llist = new DoublyLinkedList();
+
+        for (let i = 0; i < llistCount; i++) {
+            const llistItem = parseInt(readLine(), 10);
+            llist.insertNode(llistItem);
+        }
+
+        const data = parseInt(readLine(), 10);
+
+        let llist1 = sortedInsert(llist.head, data);
+
+        printDoublyLinkedList(llist1, " ", ws)
+        ws.write("\n");
+    }
+
+    ws.end();
+}
