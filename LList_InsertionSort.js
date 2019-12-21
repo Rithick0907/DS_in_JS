46c32486c90c4900b1156d7a20cf4141bf2db504
RithickB <49742736+RithickB@users.noreply.github.com>
2019-12-21 12:48:20 +0530

    Update LList_InsertionSort.js

--- LList_InsertionSort.js
+++ LList_InsertionSort.js
@@ -24,25 +24,33 @@ class LinkedList{
 }
 //Insertion Sort Function
 function Insertion(head){
+    //we create an empty list which holds the sorted data nodes in it.
    let sortedList=new LinkedList();
    let current=head;
    while(current){
+       //we store next because after inserting the node in sortedList the current.next values changes
        let next=current.next;
+       //we assign sortedList.head to access data and next property of node
        let sorted=sortedList.head;
+       //Here we check for empty (or) only one node in sorted List
        if(sorted===null||sorted.data>=current.data){
            current.next=sorted;
            sortedList.head=current;
        }
+       //if the node has value in middle (or) end of sorted list
        else{
            let prev=null;
+           //Traverse until we reach the correct position
            while(sorted.data<current.data&&sorted!==null)
            {
                prev=sorted;
                sorted=sorted.next;
            }
+           //we place the node in its right position
            prev.next=current;
            current.next=sorted;
        }
+       //we move the current pointer one step ahead
        current=next;
    }
    return sortedList;
