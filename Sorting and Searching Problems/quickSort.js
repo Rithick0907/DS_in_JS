5f066df7d9ddfa0d2056bc464ecad223ee552a28
RithickB <49742736+RithickB@users.noreply.github.com>
2019-12-25 16:38:59 +0530

    Create quickSort.js

---
+++ Sorting and Searching Problems/quickSort.js
@@ -0,0 +1,29 @@
+//It is best when the number of elements in array is less(like 10 or 100)
+function quickSort(a,low,high)
+{
+    if(low<high){
+        let q=partition(a,low,high);
+        quickSort(a,low,q-1);
+        quickSort(a,q+1,high);
+    }
+}
+function partition(a,low,high){
+    let r=a[high];
+    let i=low-1;
+    for(let j=low;j<high;j++){
+        if(a[j]<r){
+            i++;
+            let temp=a[j];
+            a[j]=a[i];
+            a[i]=temp;
+        }
+    }
+    i++;
+    let temp=a[i];
+    a[i]=a[high];
+    a[high]=temp;
+    return i;
+}
+let a=[5,2,1,0,3,7];
+quickSort(a,0,a.length-1);
+console.log(a);
